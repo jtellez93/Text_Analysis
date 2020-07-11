d7307550424e318598c47991a4af1501a488b1f7
Julian Tellez <60985751+jtellez93@users.noreply.github.com>
2020-07-11 11:51:44 -0500

    Ejemplo 1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Text_Analysis
-En este repositorio se encuentran temas relacionados con análisis de texto con R,  Text Mining, Analyzing word and document frequency, tf-idf, n-grams y topic modeling.
+En este repositorio se encuentran temas relacionados con análisis de texto con R,  Text Mining, Analyzing word and document frequency (tf-idf), n-grams y topic modeling.
--- Scripts/Ex_words_frecuency_1.R
+++ Scripts/Ex_words_frecuency_1.R
@@ -1,24 +1,51 @@
-
+library(httr)
 library(tidyverse)
 library(tokenizers)
 
 
+
 # Ejemplo 
+
+# leemos la letra de la cancion we are the champions de la siguiente url
+
+url <- "https://www.letraseningles.es/letrascanciones/traduccionesPR/Queen-WeAreTheChampions.html"
+html <- GET(url) 
+content <- content(html, as = "text") # codigo html de la pagina
+parsedHtml <- htmlParse(content, asText = TRUE)
+
+xpathSApply(parsedHtml, "//title", xmlValue)
+letra <- xpathSApply(parsedHtml, "//section[2]", xmlValue)
+
+letra <- read_lines(letra)
+letra <- letra[3:38]
+class(letra)
+
+
+# Determinar stopwords
+# son palabras que funcionan como estructura ej: el, y, la, ....
+# descargamos una lista de stopwords del lenguaje ingles basada en:
+# https://countwordsfree.com/stopwords/english/txt
+
+url_stop <- "https://countwordsfree.com/stopwords/english/txt"
+stopwords_en <- read.csv(url_stop, stringsAsFactors = F,
+                         header = F )
+stopwords_en <- stopwords_en$V1
+class(stopwords_en)
+
 
 ### segmentacion de palabras ####
 
-texto <- paste("También entiendo que como es temporada de elecciones, las expectativas para lo que lograremos este año son bajas. Aún así, señor Presidente de la Cámara de Representantes, aprecio el enfoque constructivo que usted y los otros líderes adoptaron a finales del año pasado para aprobar un presupuesto, y hacer permanentes los recortes de impuestos para las familias trabajadoras. Así que espero que este año podamos trabajar juntos en prioridades bipartidistas como la reforma de la justicia penal y ayudar a la gente que está luchando contra la adicción a fármacos de prescripción. Tal vez podamos sorprender de nuevo a los cínicos.")
+texto <- paste(letra, collapse = " ")
 texto
 
-palabras <- tokenize_words(texto)
+palabras <- tokenize_words(texto, stopwords = stopwords_en)
 palabras
 
 length(palabras) # muestra 1 dado que es una lista
 class(palabras)
 
 length(palabras[[1]])
-# [1] 101
-
+# [1] 51
 
 
 # convertir la lista en tabla 
@@ -30,23 +57,9 @@ tabla <- data_frame(palabra = names(tabla), recuento = as.numeric(tabla))
 
 # Organizar la tabla
 
-arrange(tabla, desc(tabla$recuento))
+tabla <- arrange(tabla, desc(tabla$recuento))
 
 
-
-### Detectar oraciones ####
-
-oraciones <- tokenize_sentences(texto)
-oraciones
-
-
-# tammbien podemnos dividir cada oracion por palabra
-oraciones_palabras <- tokenize_words(oraciones[[1]])
-oraciones_palabras
-
-# numero de palabras en cada oracion
-sapply(oraciones_palabras, length)
-# [1] 18 40 34  9
 
 
 
